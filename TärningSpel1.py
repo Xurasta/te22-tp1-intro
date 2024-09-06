52f4b580f7b499d6606acf12e123fe750d59cf9a
Xurasta <118819332+Xurasta@users.noreply.github.com>
2024-09-06 09:53:21 +0200

    VinONiel 2.0

--- TärningSpel1.py
+++ TärningSpel1.py
@@ -4,11 +4,22 @@ datorVärde = random.randint(1,6)
 spelarVärde = random.randint(1,6)
 
 datorVärdeMedel = int(datorVärde/2)
-print (datorVärdeMedel+random.randint(1,2))
+datorGissning = datorVärdeMedel+random.randint(1,2)
+print (datorGissning)
 
-spelarTur = string(input("Vill du gissa eller bluffa?"))
-if spelarTur == "gissa":
-    spelarGissning = int(input("Hur mycket gissar du på?"))
+spelarTur = str(input("Vill du gissa eller bluffa?"))
+while True:
+    if spelarTur == "gissa":
+        spelarGissning = int(input("Hur mycket gissar du på?"))
+        if spelarGissning < datorGissning:
+            str(input("Gissa igen"))
+    elif spelarTur == "bluffa":
+        break
+    else: 
+        spelarTur = str(input("Ogiltig input."))
+
+
+
 
 gissning = int(input("Gissning "))
 print(gissning)
